da403272b9ef81d2ca0310592eda26ca17bfc3d7
MJ-NMR <almogtbanmr123@gmail.com>
2025-01-23 11:45:23 -0500

    create tha database and rute PUT req to creat new user

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 #!/usr/bin/env node
 const express = require('express');
+const splite3 = require('sqlite3')
+const bodyParser = require('body-parser');
+const morgan = require('morgan')
 const app = express();
+const db = new  splite3.Database('./database.db');
 
-app.use((req, res, next) => {
-	res.send('<h1>From the server</h1>');
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+
+// create new envelopes
+app.put('/user', (req, res, next) => {
+	console.log(req.body);
+	const user = req.body || null;
+	if(user){
+		db.run(`INSERT INTO users (name, pass) VALUES ("${user.name}", "${user.pass}")`, (err) => {
+			if(!err){
+				res.send(`The user ${user.name} created Successfully`);
+			}else{
+				res.send(err);
+			}
+		});
+	}
+	return;
 });
 
 const port = process.env.port || 8080;
